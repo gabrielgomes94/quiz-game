bd8003223e16ce2ab969f9dddfee6f8791f5ac6e
Gabriel <gabrielgomesch@gmail.com>
2019-05-31 02:08:25 -0300

    feature(answer): added attributes for answer data

--- Assets/Scripts/AnswerData.cs
+++ Assets/Scripts/AnswerData.cs
@@ -2,17 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnswerData : MonoBehaviour
+[System.Serializable]
+public class AnswerData
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  public string answerText;
+  public bool isCorrect;
 }
