cb08320161f62509f385a3cc24f5c30301fe227d
Gabriel <gabrielgomesch@gmail.com>
2019-05-31 02:10:53 -0300

    feature(data controller): dont destroy on load

--- Assets/Scripts/DataController.cs
+++ Assets/Scripts/DataController.cs
@@ -1,18 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DataController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public RoundData[] allRoundData;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    DontDestroyOnLoad(gameObject);
+
+    SceneManager.LoadScene("MenuScreen");
+  }
+
+  public RoundData GetCurrentRoundData()
+  {
+    return allRoundData[0];
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
 }
