be845596fc794b8b79c6f8017e1e7033e29a639d
Gabriel <gabrielgomesch@gmail.com>
2019-06-02 14:28:27 -0300

    script: answer button

---
+++ Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AnswerButton : MonoBehaviour
+{
+  public Text answerText;
+
+  private AnswerData answerData;
+  // Start is called before the first frame update
+  void Start()
+  {
+      
+  }
+
+  public void Setup(AnswerData data)
+  {
+    answerData = data;
+    answerText.text = answerData.answerText;
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+      
+  }
+}
